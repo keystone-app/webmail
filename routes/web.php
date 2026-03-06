27e76a2f79397b4bb14ecbddded6e2f4804bb9e5
Marcelo Gato Lipienski <marcelo.lipienski@gmail.com>
2026-03-05 22:20:20 -0300

    feat(auth): Implement logout functionality

--- routes/web.php
+++ routes/web.php
@@ -1,13 +1,19 @@
 <?php
 
 use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\Auth\LogoutController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/login', function () {
+    return 'Login page';
+})->name('login');
+
 Route::post('/login', [LoginController::class, 'login']);
+Route::post('/logout', [LogoutController::class, 'logout'])->middleware('auth');
 
 Route::get('/home', function () {
     return 'Welcome home';
