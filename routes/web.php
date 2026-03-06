1ef84d60df062a07bde346ccf3719987bf602248
Marcelo Gato Lipienski <marcelo.lipienski@gmail.com>
2026-03-06 11:02:34 -0300

    feat(drafts): Implement drafts API store and update endpoints

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Api\AttachmentController;
+use App\Http\Controllers\Api\DraftController;
 use App\Http\Controllers\Api\EmailController;
 use App\Http\Controllers\Auth\LoginController;
 use App\Http\Controllers\Auth\LogoutController;
@@ -22,4 +23,7 @@ Route::middleware('auth')->prefix('api')->group(function () {
     Route::get('/emails', [EmailController::class, 'index']);
     Route::get('/emails/{email}', [EmailController::class, 'show']);
     Route::get('/attachments/{attachment}', [AttachmentController::class, 'show'])->name('attachments.show');
+    
+    Route::post('/drafts', [DraftController::class, 'store']);
+    Route::put('/drafts/{draft}', [DraftController::class, 'update']);
 });
